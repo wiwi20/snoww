30937a0ed1eaa368d3bff7fcab8b2bcf5cfc90d6
wiwi20 <59695931+wiwi20@users.noreply.github.com>
2021-06-09 11:36:21 +0200

    Add files via upload

---
+++ js/rom.js
@@ -0,0 +1,221 @@
+
+$(document).on("click", function(e){
+    if($(e.target).is("#white")){
+      $("#photo1-1").show();
+      $("#photo1").hide();
+      $("#photo1-2").hide();
+      $("#photo1-3").hide();
+    }
+    if($(e.target).is("#brown")){
+      $("#photo1").show();
+      $("#photo1-1").hide();
+      $("#photo1-2").hide();
+      $("#photo1-3").hide();
+    }
+    if($(e.target).is("#yellow")){
+      $("#photo1-2").show();
+      $("#photo1-1").hide();
+      $("#photo1").hide();
+      $("#photo1-3").hide();
+    }
+       if($(e.target).is("#light-brown")){
+      $("#photo1-3").show();
+      $("#photo1-1").hide();
+      $("#photo1").hide();
+      $("#photo1-2").hide();
+    }
+
+
+    if($(e.target).is("#white2")){
+      $("#photo2-1").show();
+      $("#photo2").hide();
+      $("#photo2-2").hide();
+      $("#photo2-3").hide();
+    }
+    if($(e.target).is("#brown2")){
+      $("#photo2").show();
+      $("#photo2-1").hide();
+      $("#photo2-2").hide();
+      $("#photo2-3").hide();
+    }
+    if($(e.target).is("#yellow2")){
+      $("#photo2-2").show();
+      $("#photo2-1").hide();
+      $("#photo2").hide();
+      $("#photo2-3").hide();
+    }
+       if($(e.target).is("#light-brown2")){
+      $("#photo2-3").show();
+      $("#photo2-1").hide();
+      $("#photo2-2").hide();
+      $("#photo2").hide();
+    }
+
+
+
+    if($(e.target).is("#white3")){
+      $("#photo3-3").show();
+      $("#photo3-1").hide();
+      $("#photo3-2").hide();
+      $("#photo3").hide();
+    }
+    if($(e.target).is("#brown3")){
+      $("#photo3").show();
+      $("#photo3-1").hide();
+      $("#photo3-2").hide();
+      $("#photo3-3").hide();
+    }
+    if($(e.target).is("#yellow3")){
+      $("#photo3-1").show();
+      $("#photo3-2").hide();
+      $("#photo3").hide();
+      $("#photo3-3").hide();
+    }
+       if($(e.target).is("#light-brown3")){
+      $("#photo3-2").show();
+      $("#photo3-1").hide();
+      $("#photo3-3").hide();
+      $("#photo3").hide();
+    }
+
+
+
+    if($(e.target).is("#white4")){
+      $("#photo4-3").show();
+      $("#photo4-1").hide();
+      $("#photo4-2").hide();
+      $("#photo4").hide();
+    }
+    if($(e.target).is("#brown4")){
+      $("#photo4").show();
+      $("#photo4-1").hide();
+      $("#photo4-2").hide();
+      $("#photo4-3").hide();
+    }
+    if($(e.target).is("#yellow4")){
+      $("#photo4-1").show();
+      $("#photo4-2").hide();
+      $("#photo4").hide();
+      $("#photo4-3").hide();
+    }
+       if($(e.target).is("#light-brown4")){
+      $("#photo4-2").show();
+      $("#photo4-1").hide();
+      $("#photo4-3").hide();
+      $("#photo4").hide();
+    }
+
+
+
+      if($(e.target).is("#black5")){
+      $("#photo5").show();
+      $("#photo5-1").hide();
+      $("#photo5-2").hide();
+      $("#photo5-3").hide();
+      $("#photo5-4").hide();
+    }
+
+    if($(e.target).is("#light-brown5")){
+      $("#photo5-3").show();
+      $("#photo5-1").hide();
+      $("#photo5-2").hide();
+      $("#photo5-4").hide();
+      $("#photo5").hide();
+  }
+    
+    if($(e.target).is("#red5")){
+      $("#photo5-2").show();
+      $("#photo5-3").hide();
+      $("#photo5-4").hide();
+      $("#photo5-1").hide();
+      $("#photo5").hide();
+    }
+    if($(e.target).is("#blue5")){
+      $("#photo5-1").show();
+      $("#photo5-3").hide();
+      $("#photo5-4").hide();
+      $("#photo5-2").hide();
+      $("#photo5").hide();
+    }
+       if($(e.target).is("#yellow5")){
+      $("#photo5-4").show();
+      $("#photo5-3").hide();
+      $("#photo5-2").hide();
+      $("#photo5-1").hide();
+      $("#photo5").hide();
+    }
+
+
+     if($(e.target).is("#white6")){
+      $("#photo6-1").show();
+      $("#photo6").hide();
+  }
+      if($(e.target).is("#black6")){
+      $("#photo6").show();
+      $("#photo6-1").hide();
+  }
+
+
+       if($(e.target).is("#white7")){
+      $("#photo7-1").show();
+      $("#photo7").hide();
+  }
+      if($(e.target).is("#black7")){
+      $("#photo7").show();
+      $("#photo7-1").hide();
+  }
+
+
+
+      if($(e.target).is("")){
+      $("#photo8").show();
+      $("#photo8-1").hide();
+      $("#photo8-2").hide();
+  }
+      if($(e.target).is("#red8")){
+      $("#photo8-1").show();
+      $("#photo8").hide();
+      $("#photo8-2").hide();
+  }
+      if($(e.target).is("#blue8")){
+      $("#photo8-2").show();
+      $("#photo8").hide();
+      $("#photo8-1").hide();
+}
+
+
+    if($(e.target).is("#white9")){
+      $("#photo9").show();
+      $("#photo9-1").hide();
+      $("#photo9-2").hide();
+      $("#photo9-3").hide();
+    }
+    if($(e.target).is("#brown9")){
+      $("#photo9-3").show();
+      $("#photo9-1").hide();
+      $("#photo9-2").hide();
+      $("#photo9").hide();
+    }
+    if($(e.target).is("#yellow9")){
+      $("#photo9-1").show();
+      $("#photo9-2").hide();
+      $("#photo9").hide();
+      $("#photo9-3").hide();
+    }
+       if($(e.target).is("#light-brown9")){
+      $("#photo9-2").show();
+      $("#photo9-1").hide();
+      $("#photo9").hide();
+      $("#photo9-3").hide();
+    }
+
+
+      if($(e.target).is("#white10")){
+      $("#photo10").show();
+      $("#photo10-1").hide();
+  }
+      if($(e.target).is("#brown10")){
+      $("#photo10-1").show();
+      $("#photo10").hide();
+  }
+});
